7f11adbf67c883705061c71a12d7bbc0504dfec5
AG <ozyohthree@gmail.com>
2025-11-06 22:29:10 +0000

    updates for debugging

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
     { id: 2, name: 'Mouse', price: 25 },
     { id: 3, name: 'Keyboard', price: 75 },
     { id: 4, name: 'Monitor', price: 300 },
+    { id: 5, name: 'Stand', price: 100 }
+
   ]);
 
   const [cart, setCart] = useState([]);
